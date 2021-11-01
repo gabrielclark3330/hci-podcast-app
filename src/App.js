8e2d6199391f403fa85d553c5e95bf8c569e0608
Gabriel Clark <>
2021-11-01 17:10:00 -0500

    Deploy test.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { GenreInputField } from './components/SearchBar.js';
+import { useState } from 'react'
 
 function App() {
+  let enteredString = "";
+  const [state, setState]= useState({
+    enteredGenre: enteredString,
+  });
   return (
     <div className="App">
+      <GenreInputField/>
+      <h1></h1>
       <h2>GABRIEL IS UP AND LIIIIIIVEEE BABY!!!!!</h2>
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,2 +1,22 @@
 import React, {useState} from "react";
 
+export function GenreInputField(props) {
+  let enteredString = "";
+  const [state, setState]= useState({
+    enteredGenre: enteredString,
+  });
+  function handleChange(e) {
+    enteredString = e.target.value;
+    setState({
+      enteredGenre: enteredString,
+    });
+  }
+
+  return (
+    <div className="inputFieldContainer">
+      <label for="inputField">inputField {state.enteredGenre} </label>
+      <input id="inputField" className="inputField" type="text"
+        placeholder={ "Genres and Subgenres" } onChange={handleChange}/>
+    </div>
+  );
+}
